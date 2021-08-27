af90f34991af44b9d4b3dc560805bcb09fabf35c
KAMILY VITORIA TOSTA <21192995@CFP127ALUNO.LOCAL>
2021-08-27 10:20:20 -0300

    adicionando

---
+++ alunos/index.php
@@ -0,0 +1,34 @@
+<?php
+
+    require("./alunos.php");
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet"/>
+    <title>Notas dos alunos</title>
+</head>
+<body>
+    <section>
+    <h1>Tabela de Notas</h1>
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Nota</th>
+            <th>Situação</th>
+        </tr>
+        <tr>
+            <td>Fulano</td>
+            <td>65</td>
+            <td>100</td>
+            <td>Aprovado</td>
+        </tr>
+    </table>
+</section>
+</body>
+</html>
